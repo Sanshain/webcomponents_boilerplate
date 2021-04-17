db9eb56695192d8a78cc51f9dba5c4935b58db11
Sanshain <digital-mag@ya.ru>
2021-04-17 10:00:40 +0300

    classes ie support via bringing js code of class definition to independent file translated to es5 by some bundler with babel

--- legacy__ce_sd__support/es5__ce__polyfill.js
+++ legacy__ce_sd__support/es5__ce__polyfill.js
@@ -1,20 +1,68 @@
-if (!Array.from){
 
-    window.addEventListener(window.loadEvent || 'DOMContentLoaded', function() {
-    
-        window.customElements = window.customElements || {};
-        customElements.define = function(componentName, template){
-            
-            var sample = template.cloneNode(true);  // let
-            sample.style.display = 'block';
-            [].slice.apply(document.querySelectorAll(componentName)).forEach(function(elem){
-                
-                var slot = elem.innerHTML; 
-                elem.innerHTML = slot 
-                    ? sample.innerHTML.replace(/<slot>[\s\S]*?<\/slot>/, slot)        // вставляем slot
-                    : sample.innerHTML;
-            });                            
-        }                
-            
-    });
+/**
+ * custom (my) polyfill
+ */
+if (!Array.from) {
+
+
+	// Object.assign = function (baseObj, extObj) {
+	// 	for (const key in extObj) {
+	// 		if (Object.hasOwnProperty.call(extObj, key)) {
+	// 			baseObj[key] = extObj[key];
+	// 		}
+	// 	}
+	// }
+
+
+	window.addEventListener(window.loadEvent || 'DOMContentLoaded', function () {
+
+		window.customElements = window.customElements || {};
+
+		/**
+		 * копирует содержимое темплейта в новую ноду со вставкой содержимого слота
+		 */
+		customElements.define = function (componentName, template) {
+
+			var sample = template.cloneNode(true);  // let
+			sample.style.display = 'block';
+			[].slice.apply(document.querySelectorAll(componentName)).forEach(function (elem) {
+
+				var slot = elem.innerHTML;
+				elem.innerHTML = slot
+					? sample.innerHTML.replace(/<slot>[\s\S]*?<\/slot>/, slot)        // вставляем slot
+					: sample.innerHTML;
+			});
+		}
+
+		/**
+		 * 
+		 * @param {string} component - имя компонента, должно совпадать с id template, в котором он описан
+		 * @param {HTMLElementPrototype | HTMLTemplateElement} componentPrototype - прототип веб-компонента (созданный экз-р класса вебкомп-та,
+		 * 										  сбандленного бабелем в ес5 и присоединенного динамически перед этим бандлом либо template
+		 * 										  (последний подходит только для styled components, не более))
+		 */
+		customElements.define = function (component, componentPrototype) {
+
+			var template = document.getElementById(component);
+			
+			// если component не найден, значит такого шаблона нет - значит это просто стайлед компонент
+			if (!template) template = componentPrototype;
+			// если найден, то копируем в него свойства и методы, заданные в прототипе
+			else {
+				// template.call(componentPrototype);	- // не катит, т.к. у элементов нельзя вызвать конструктор вручную
+				Object.assign(template, componentPrototype)
+			}
+
+			var sample = element.cloneNode(true);  // let
+			sample.style.display = 'block';
+			[].slice.apply(document.querySelectorAll(component)).forEach(function (elem) {
+
+				var slot = elem.innerHTML;
+				elem.innerHTML = slot
+					? sample.innerHTML.replace(/<slot>[\s\S]*?<\/slot>/, slot)        // вставляем slot
+					: sample.innerHTML;
+			});
+		}
+
+	});
 }
--- legacy__ce_sd__support/shadowcss__polyfill.js
+++ legacy__ce_sd__support/shadowcss__polyfill.js
@@ -1,20 +1,33 @@
-this.attachShadow || window.addEventListener(window.loadEvent || "DOMContentLoaded", function() {
 
-    var style = document.createElement("style");
-    style.type = "text/css";
-    document.head.appendChild(style);
 
-    [].slice.apply(document.querySelectorAll("template")).forEach(function(template) {
+/**
+ * my custom polyfill?
+ */
+this.attachShadow || window.addEventListener(window.loadEvent || "DOMContentLoaded", function () {
 
-        var shadowedCss = template.innerHTML.replace(/<style>([\s\S]*?)<\/style>/, function(r, group) 
-        {
-            return group.replace(/(\n\s*)([\.#\w\s]*){/g, function(_, offset, selector) {
-                return offset + "." + template.id + " " + selector + "{"
-            }).replace(/\:host/g, "." + template.id)
-        });
+	// создает тег style в head, который будет отвечать за стили элемента
+	var style = document.createElement("style");
+	style.type = "text/css";
+	document.head.appendChild(style);
 
-        if (style.styleSheet) style.styleSheet.cssText = shadowedCss;
-        else style.appendChild(document.createTextNode(shadowedCss));        
-    })
-    
+	[].slice.apply(document.querySelectorAll("template")).forEach(function (template) {
+
+		var shadowedCss = '';
+		var html = template.innerHTML.replace(/<style>([\s\S]*?)<\/style>/, function (r, group) {
+			
+			// добавляет id шаблона к каждому селектору
+			shadowedCss += group.replace(/(\n\s*)([\.#\w\s]*){/g, function (_, offset, selector) {
+				return offset + "." + template.id + " " + selector + "{"
+			}).replace(/\:host/g, "." + template.id)
+			return '';
+		});
+
+		// удаляет стили из шаблона
+		[].slice.call(template.querySelectorAll('style')).forEach(function(el) {el.parentElement.removeChild(el)});
+
+		// добавляет стили в шапку
+		if (style.styleSheet) style.styleSheet.cssText += shadowedCss; // shadowedCss;
+		else style.appendChild(document.createTextNode(shadowedCss));
+	})
+
 });
